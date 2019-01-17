7ffa68ee55a450fbb7006a614dd14bc1ee8f0a0b
unknown <wnsaud6016@naver.com>
2019-01-17 21:44:43 +0900

    insert 문

--- chap07.db/db-exam/routes/show-data.js
+++ chap07.db/db-exam/routes/show-data.js
@@ -19,4 +19,21 @@ router.get('/', function (req, res, next) {
     });
 });
 
+router.get('/insert',function(req,res,next){
+    const query = `INSERT INTO USER(id,name) VALUES (?) `;
+    let value=[999,"이준명"]
+    mysqlDB.query(query,[value], function (err, rows, fields) {
+        if (!err) {
+            console.log(rows);
+            console.log(fields);
+            var result = 'rows : ' + JSON.stringify(rows) + '<br><br>' +
+                'fields : ' + JSON.stringify(fields);
+            res.send(result);
+        } else {    
+            console.log('query error : ' + err);
+            res.send(err);
+        }
+    });
+});
+
 module.exports = router;
